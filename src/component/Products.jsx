387fed75f358859a51b138160d660d7e5df1ff94
unknown <superartshop01@gmail.com>
2024-03-24 11:43:47 +0530

    fix

--- src/component/Products.jsx
+++ src/component/Products.jsx
@@ -1,36 +1,31 @@
 import { useEffect, useState } from "react";
 import MealItem from "./MealItem";
+import productjson from "../util/products.json";
 
-const Products = () =>{
+const Products = () => {
+  const [products, setProucts] = useState([]);
 
-    const [products,setProucts] = useState([]);
+  useEffect(() => {
+    async function getProducts() {
+      //console.log(productjson);
+      //   const response = await fetch("http://localhost:3000/meals/");
+      //   const resData = await response.json();
 
-    useEffect(()=>{
+      setProucts(productjson);
+    }
 
-        async function getProducts(){
-            const response = await fetch ("http://localhost:3000/meals/");
-            const resData = await response.json();
-            setProucts(resData);          
-        }
-
-        getProducts();
-    },[])
-    return(
-        <>
-            {
-                <ul id="meals">
-                    {
-                        
-                        products.map((product)=>{
-                            return (
-                                <MealItem meal={product} key={product.id} />
-                            )
-                        })
-                    }
-                </ul>
-            }
-                
-        </>
-    )
-}
+    getProducts();
+  }, []);
+  return (
+    <>
+      {
+        <ul id="meals">
+          {products.map((product) => {
+            return <MealItem meal={product} key={product.id} />;
+          })}
+        </ul>
+      }
+    </>
+  );
+};
 export default Products;
